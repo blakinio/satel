979fa9134d31214dd30961a6a743477b4a76dae0
blakinio <75369544+blakinio@users.noreply.github.com>
2025-08-03 18:48:30 +0200

    Add device info for Satel entities

--- custom_components/satel/switch.py
+++ custom_components/satel/switch.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import DeviceInfo
 
 from . import SatelHub
 from .const import DOMAIN
@@ -32,6 +33,15 @@ class SatelOutputSwitch(SwitchEntity):
         self._attr_name = name
         self._attr_is_on = False
         self._attr_unique_id = f"satel_output_{output_id}"
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return device information for this entity."""
+        return DeviceInfo(
+            identifiers={(DOMAIN, self._hub.host)},
+            manufacturer="Satel",
+            name="Satel Alarm",
+        )
 
     async def async_turn_on(self, **kwargs) -> None:
         await self._hub.send_command(f"OUTPUT {self._output_id} ON")
